6d9540af9f81f23fd1038fbe655ba40ce71e0c32
gpapa-sam <frbrigadas@gmail.com>
2024-02-09 09:41:46 -0300

    leitura e daptação de arquivo csv

--- dispesas_pessoais.py
+++ dispesas_pessoais.py
@@ -1,11 +1,12 @@
-ano = [{'salario': {1: 17785.91},
-        'nomad': {5: 1000, 8: 1001},
-       'condominio': {8: 455},
-        'mei': {17: 72}}]
+from csv import DictReader, reader
+
+
+ano = [[], [], [], [], [], [], [], [], [], [], [],[]]
 
 meses = ['janeiro', 'fevereiro', 'março', 'abril', 'maio', 'junho', 'julho', 'agosto', 'setembro',
          'outubro', 'novembro', 'dezembro']
 
+"""
 print(f'mês de {meses[0]}')
 print()
 print(f'gasto\t\t\t\tdia\t\tvalor(R$)')
@@ -15,3 +16,36 @@ for mes in ano:
         for key, value in valor.items():
             #print(gasto, '\t\t\t', key, '\t', value)
             print(f'{gasto:<10} {key:>12} \t {value}')
+
+
+with open("orc_23.csv") as arquivo:
+    mes = DictReader(arquivo, delimiter=',')
+    for dia in mes:
+
+        #print(f"{dia['data']} - {dia['objeto']} - {dia['valor']}")
+        print(dia)
+
+"""
+with open('orc_23.csv') as arquivo:
+    leitor_csv = reader(arquivo)
+    #next(leitor_csv)  # Pular o cabeçalho
+    for indice, linha in enumerate(leitor_csv):
+
+        m = []
+        for i, x in enumerate(linha):
+
+            m.append(linha[i])
+            if (i+1) % 3 == 0:
+                if indice == 0:
+                    #z = list
+                    #z.append(m)
+                    ano[i//3].append(m)
+                    m = []
+                    print(ano)
+                else:
+                    ano[i//3].append(m)
+                    m = []
+
+
+
+print(ano)
