961756687bcb6aa93e1a163a13f36285ccb540a9
gpapa-sam <frbrigadas@gmail.com>
2024-02-29 07:19:33 -0300

    ajustado carregamento e escrita em arquivos csv

--- dispesas_pessoais.py
+++ dispesas_pessoais.py
@@ -1,4 +1,4 @@
-from csv import DictReader, reader
+from csv import DictReader, reader, writer
 
 
 ano = [[], [], [], [], [], [], [], [], [], [], [],[]]
@@ -7,41 +7,26 @@ meses = ['janeiro', 'fevereiro', 'março', 'abril', 'maio', 'junho', 'julho', 'a
          'outubro', 'novembro', 'dezembro']
 
 """
-print(f'mês de {meses[0]}')
-print()
-print(f'gasto\t\t\t\tdia\t\tvalor(R$)')
-for mes in ano:
-    for gasto, valor in mes.items():
-        #print(gasto, '-', valor)
-        for key, value in valor.items():
-            #print(gasto, '\t\t\t', key, '\t', value)
-            print(f'{gasto:<10} {key:>12} \t {value}')
 
 
-with open("orc_23.csv") as arquivo:
-    mes = DictReader(arquivo, delimiter=',')
-    for dia in mes:
 
-        #print(f"{dia['data']} - {dia['objeto']} - {dia['valor']}")
-        print(dia)
-
-"""
 with open('orc_23.csv') as arquivo:
-    leitor_csv = reader(arquivo)
-    #next(leitor_csv)  # Pular o cabeçalho
+    leitor_csv = reader(arquivo) # cada linha é uma lista
+
     for indice, linha in enumerate(leitor_csv):
 
-        m = []
+        m = [] # uma lista temporária
+        # aqui faz o loop de cada linha do arquivo que tem 36 argumento(3 por mês) contabilizando um ano
         for i, x in enumerate(linha):
 
             m.append(linha[i])
-            if (i+1) % 3 == 0:
-                if indice == 0:
+            if (i+1) % 3 == 0: #inicio de cada mês ( a cada 3 campos)
+                if indice == 0: #somente se for a primeira linha
                     #z = list
                     #z.append(m)
                     ano[i//3].append(m)
                     m = []
-                    print(ano)
+                    #print(ano)
                 else:
                     ano[i//3].append(m)
                     m = []
@@ -49,3 +34,23 @@ with open('orc_23.csv') as arquivo:
 
 
 print(ano)
+print(f'mês de {meses[0]}')
+print()
+print(f'data\t\t\t{"gasto":<30} valor(R$)')
+print()
+for gasto in ano[0]:
+    print(f'{gasto[0]}\t\t{gasto[1]:<30} {gasto[2]:>15}')
+"""
+#escrevendo os dados em arquivo csv
+with open('janeiro.csv', 'a') as file:
+    escritor_csv = writer(file)
+    sair = None
+    #escritor_csv.writerow(['data', 'gasto', 'valor'])
+    while sair != 's':
+        data = input('informe a dia do gasto : ') + '/jan.'
+        gasto = input('informe a descrição do gasto: ')
+        valor = float(input('informe  o valor do gasto: '))
+        sair = input('deseja continuar? (s/n): ')
+        escritor_csv.writerow([data, gasto, valor])
+
+
